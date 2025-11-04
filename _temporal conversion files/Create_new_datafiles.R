7de341e68d041d7b39ef0930f821c89b912edb62
Javier Del Águila Mejía <javidelaguila@gmail.com>
2025-11-04 16:22:32 +0100

    initial commit
    Added previous materials

---
+++ _temporal conversion files/Create_new_datafiles.R
@@ -0,0 +1,33 @@
+pacman::p_load(tidyverse, rio, here)
+
+# Practical 3
+dis3 <- import(here("_temporal conversion files", "tsa_stata.dta"))   # original data
+write_csv2(dis3, here("data", "tsa_pumala.csv"))
+
+# Practical 4
+mort <- import(here("_temporal conversion files/mortality.dta"))    # original data
+
+communlab <- c(
+  "Andalucía", "Aragón", "Asturias", "Baleares",
+  "Canarias", "Cantabria", "Castilla y León",
+  "Castilla-La Mancha", "Cataluña",
+  "Comunidad Valenciana", "Extremadura",
+  "Galicia", "Madrid", "Murcia", "Navarra",
+  "País Vasco", "La Rioja"
+)
+
+mort <-
+  mort %>%
+  mutate(community2 = ordered(community, labels = communlab))
+
+mortagg <-
+  mort %>%
+  group_by(year, week) %>% 
+  summarise(
+    cases = sum(cases, na.rm = T),
+    cases_m = sum(cases_m, na.rm = T),
+    cases_f = sum(cases_f, na.rm = T)
+  )
+
+
+write_csv2(mortagg, here("data", "mortagg.csv"))
