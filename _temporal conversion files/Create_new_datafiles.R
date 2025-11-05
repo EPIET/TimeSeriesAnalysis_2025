f5d51107f57691ae7bb798a88fcee9c583db2b22
Javier Del Águila Mejía <javidelaguila@gmail.com>
2025-11-05 11:58:55 +0100

    update script 7

--- _temporal conversion files/Create_new_datafiles.R
+++ _temporal conversion files/Create_new_datafiles.R
@@ -27,7 +27,11 @@ mortagg <-
     cases = sum(cases, na.rm = T),
     cases_m = sum(cases_m, na.rm = T),
     cases_f = sum(cases_f, na.rm = T)
-  )
+  ) %>% 
+  ungroup() %>% 
+  mutate(pop = rep(c(39953520, 40688520, 41423520, 42196231, 42859172,    
+                     43662613,  44360521, 45236004, 45983169, 46367550), 
+                   each = 52))
 
 write_csv2(mortagg, here("data", "mortagg.csv"))
 
